76823d01a3fc42b55b7e16d1916c5f8127d81069
Davis Pham <arcentalminitte@gmail.com>
2018-02-09 16:19:39 -0800

    Fixed class declaration

--- application/models/EquipmentSet.php
+++ application/models/EquipmentSet.php
@@ -2,7 +2,7 @@
 /**
 Model to access the equipment set table
 */
-class Category extends CVS_Model {
+class EquipmentSet extends CSV_Model {
     
     function __construct() {
         parent::__construct('../items/equipmentPresets.csv','id');
