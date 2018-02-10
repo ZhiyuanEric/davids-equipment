d5f67c202e7e186732cc86d53115296724cc8eb0
Minitte <arcentalminitte@gmail.com>
2018-02-09 19:25:51 -0800

    Revert "Revert "Models finally loaded...""

--- application/models/Accessory.php
+++ application/models/Accessory.php
@@ -5,6 +5,6 @@ Model to access the accessory table
 class Accessory extends CSV_Model {
     
     function __construct() {
-        parent::__construct('../items/accessory.csv','id');
+        parent::__construct('../data/accessory.csv','id');
     }
 }
