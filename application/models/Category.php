725698205e42bb3bc70026ad9a5febabe8dcf8a7
Minitte <arcentalminitte@gmail.com>
2018-02-09 19:20:00 -0800

    Revert "Models finally loaded..."

--- application/models/Category.php
+++ application/models/Category.php
@@ -5,6 +5,6 @@ Model to access the categories table
 class Category extends CSV_Model {
     
     function __construct() {
-        parent::__construct('../data/category.csv','id');
+        parent::__construct('../items/category.csv','id');
     }
 }
